0e13ba5e6afe82771472ddf04a4b14dba737644a
Dharani <pallapudharani19@gmail.com>
2026-06-15 20:23:58 +0530

    fix backend syntax error

--- backend/server.js
+++ backend/server.js
@@ -130,5 +130,5 @@ app.get("/create-table", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(✅ Server running on port ${PORT});
+  console.log(`✅ Server running on port ${PORT}`);
 });
